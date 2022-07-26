fd75b91b6ef078f51e970bef1a7e32e9245483c0
LITTLESITE <108331059+LITTLESITE@users.noreply.github.com>
2022-07-26 15:14:24 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -38,7 +38,7 @@ module.exports = {
   // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
   // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
   footer:
-    '<iframe name=\"toppage\" width=100% height=100% marginwidth=0 marginheight=0 frameborder=\"no\" border=\"0\"  src=\"waline.html\" ></iframe>',
+    '<iframe name=\"toppage\" width=100% height=100% marginwidth=0 marginheight=0 frameborder=\"no\" border=\"0\"  src=\"./waline.html\" ></iframe>',
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
